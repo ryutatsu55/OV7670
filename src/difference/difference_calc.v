16af32ff581cd292ed8f27381b47164063dea85d
kei-t18 <295329353+kei-t18@users.noreply.github.com>
2026-06-25 18:45:33 +0900

    テスト

--- src/difference/difference_calc.v
+++ src/difference/difference_calc.v
@@ -13,14 +13,21 @@ module difference_calc(
   output reg  [7:0]  prev_wr_data,
   output reg         prev_wr_we,
 
-  output reg  [18:0] diff_addr,
+  output reg  [18:0] diff_addr, //19bit
   output reg  [7:0]  diff_data,
   output reg         diff_we
 );
 
+reg f_dist; // 0: frame0 , 1: frame1
+
+localparam BASE = 18'd76800; //320*240 = 76800
+localparam THRESHOLD = 8'd20; // 差分の閾値
+
 reg [18:0] current_addr_d1, current_addr_d2;
 reg [7:0]  current_data_d1, current_data_d2;
 reg        current_we_d1, current_we_d2;
+
+reg [31:0] Sx, Sy, S; // 重心計算用の変数
 
 always @(posedge clock) begin
   if (reset) begin
@@ -53,14 +60,39 @@ always @(posedge clock) begin
     current_data_d2 <= current_data_d1;
     current_we_d2   <= current_we_d1;
 
+
     // 差分出力
-    diff_addr <= current_addr_d2;
+    if(f_dist ==1'b1)begin
+      diff_addr <= current_addr_d2 + BASE;
+    end else begin
+      diff_addr <= current_addr_d2;
+    end
+
+    if(current_addr_d2 == 19'd76799)begin
+      f_dist <= ~f_dist;
+    end
+
     diff_we   <= current_we_d2;
 
     if (current_data_d2 > prev_rd_data)
       diff_data <= current_data_d2 - prev_rd_data;
     else
       diff_data <= prev_rd_data - current_data_d2;
+
+    //TODO: Sx Sy Sの計算
+    if(diff_data > THRESHOLD)begin
+      // 差分が閾値を超えた場合の処理
+      Sx <= Sx + current_addr_d2[18:0];
+      Sy <= Sy + 19'd239 - current_addr_d2[18:0];
+      S <= S + 1;
+
+    end
+    else begin
+      // 差分が閾値以下の場合の処理
+
+
+    end
+    //TODO: 重心の算出
 
     // 現在フレームを次回用に保存
     prev_wr_addr <= current_addr_d2;
